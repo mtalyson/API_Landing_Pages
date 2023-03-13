1e03f318ad4020eca6ddb93802bc377b457fa1ba
Talyson Moreira <talyson.penha@gmail.com>
2023-03-13 16:42:13 -0300

    first commit

---
+++ config/admin.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
+
+module.exports = ({ env }) => ({
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
+});
---
+++ config/database.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
+
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+
+  const connections = {
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          '..',
+          env('DATABASE_FILENAME', 'data.db'),
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
